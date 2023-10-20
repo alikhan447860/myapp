9826f547edb7420f70d5c75c019128df8e2f1ac9
Ali khan <khanali6861@gmail.com>
2023-10-20 04:41:13 -0700

    kaam jari he

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name ="Ali khan"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <h1>My Name is {name}</h1>
+      <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Officiis mollitia accusantium neque cupiditate? 
+       Cupiditate temporibus adipisci necessitatibus, sapiente ex laborum ratione esse culpa iusto inventore cumque hic repellendus quam commodi itaque dolore praesentium. Odit.</div>
+    </>
   );
 }
 
