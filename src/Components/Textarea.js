0b0b8ed2d8cc445523cdb1c7685b79d9daa9b549
Ali khan <khanali6861@gmail.com>
2023-10-21 07:10:24 -0700

    lower case joda

--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -6,18 +6,33 @@ export default function Textarea(props) {
       let newtext=text.toUpperCase();
       setText(newtext);
     }
+    const handleloclick=()=>{
+        //   console.log("you have click the change button"+text);
+          let newtext=text.toLowerCase();
+          setText(newtext);
+        }
     const handleonchange=(event)=>{
         // console.log("you have change");
         setText(event.target.value);
 
     }
-    const[text,setText]=useState("Enter the text here")
-  return (
+    const[text,setText]=useState("Enteclar the text here")
+  return (<>
 
-<div class="form-floating">
+<div className="container" ss="form-floating">
 <h1>{props.heading}</h1>
-  <textarea class="form-control"  value={text} onChange={handleonchange} placeholder="Leave a comment here" id="floatingTextarea" rows="8"></textarea><br />
-  <button className="btn btn-primary" onClick={handleupclick}>Convert to Upercase</button>
+  <textarea className="form-control"  value={text} onChange={handleonchange} placeholder="Leave a comment here" id="floatingTextarea" rows="8"></textarea><br />
+  <button className="btn btn-outline-success mx-2" onClick={handleupclick}>Convert to Upercase</button>
+  <button className="btn btn-outline-success mx-2" onClick={handleloclick}>Convert to lowercase</button>
+
 </div>
+  <div className="container my-4" >
+    <h1>Your Text Summary is here</h1>
+    <p>{text.split(" ").length}words and {text.length} characters</p>
+    <p>{0.008*text.split("").length}  min to read</p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+  </div>
+</>
   )
 }
