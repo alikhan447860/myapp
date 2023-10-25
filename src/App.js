413aa3ac66547aa3a0a440cc3020351fd48a1b78
Ali khan <khanali6861@gmail.com>
2023-10-25 05:04:14 -0700

    again deploy

--- src/App.js
+++ src/App.js
@@ -3,31 +3,21 @@ import Navbar from './Components/Navbar';
 import Textarea from './Components/Textarea';
 import About from './Components/About'
 // import { useState } from 'react';\
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+
 function App() {
   return (
     <>
-    <Router>
+    
  <Navbar title="TextUtils" about="About Us" />
  <div className="container my-3">
- <Switch>
-      <Route exact path="/">
+ 
       <Textarea heading="Enter The Text here"/>
   
-      </Route>
-      <Route path="/about">
+  
         <About />
-      </Route>
-
-    </Switch>
 
  </div>
- </Router>
-  <About/>
+ 
 
     </>
   );
